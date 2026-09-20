9a51b05f45c3b0bba1ea9245ab14c8745836ea58
Lino Thebault - Tiruu <97034382+Tiruu@users.noreply.github.com>
2026-09-20 17:30:29 +0200

    Register project workflow and GitHub commands

--- src/discord/registerCommands.ts
+++ src/discord/registerCommands.ts
@@ -6,6 +6,13 @@ import { projectShowCommand } from "./commands/projectShow.js";
 import { projectTaskCommand } from "./commands/projectTask.js";
 import { projectDoneCommand } from "./commands/projectDone.js";
 import { projectCreateCommand } from "./commands/projectCreate.js";
+import { projectStatusCommand } from "./commands/projectStatus.js";
+import { projectDecisionCommand } from "./commands/projectDecision.js";
+import { projectNoteCommand } from "./commands/projectNote.js";
+import { projectStartCommand } from "./commands/projectStart.js";
+import { projectNextCommand } from "./commands/projectNext.js";
+import { projectGithubCommand } from "./commands/projectGithub.js";
+import { projectGithubSyncCommand } from "./commands/projectGithubSync.js";
 
 const token = process.env.DISCORD_TOKEN;
 const clientId = process.env.DISCORD_CLIENT_ID;
@@ -31,6 +38,13 @@ await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
     projectTaskCommand.data.toJSON(),
     projectDoneCommand.data.toJSON(),
     projectCreateCommand.data.toJSON(),
+    projectStatusCommand.data.toJSON(),
+    projectDecisionCommand.data.toJSON(),
+    projectNoteCommand.data.toJSON(),
+    projectStartCommand.data.toJSON(),
+    projectNextCommand.data.toJSON(),
+    projectGithubCommand.data.toJSON(),
+    projectGithubSyncCommand.data.toJSON(),
   ],
 });
 
