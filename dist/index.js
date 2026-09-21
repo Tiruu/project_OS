a6e2dd8631b71eb25fcf47a082acba8ad226c9f3
Lino Thebault - Tiruu <97034382+Tiruu@users.noreply.github.com>
2026-09-21 14:27:06 +0200

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import { discordClient, startDiscord } from "./lib/discord.js";
-try {
-    await startDiscord();
-    console.log(`Discord connecté en tant que ${discordClient.user?.tag}`);
-}
-catch (error) {
-    console.error(error);
-}
